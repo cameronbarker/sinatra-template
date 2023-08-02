555edef9991fe53d15d850b592751f91e7d86891
Cameron Barker <barker.cameron@gmail.com>
2023-08-01 22:05:58 -0700

    Migrating and cleaning up db

--- app.rb
+++ app.rb
@@ -12,13 +12,12 @@ class App < Sinatra::Base
   configure do
     set :root, File.dirname(__FILE__)
     set :public_folder, "public"
-    set :database, {adapter: "sqlite3", database: "db.sqlite3"}
 
     register Sinatra::ActiveRecordExtension
   end
 
   configure :development do
-    set :database, "sqlite3:forum-service.sqlite3"
+    set :database, {adapter: "sqlite3", database: "db.sqlite3"}
 
     register Sinatra::Reloader
   end
